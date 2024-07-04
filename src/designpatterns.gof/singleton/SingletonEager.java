888d7c6c091afe0fdcb268f4f5e99253d9803908
azoozin <mario.github@protonmail.com>
2024-07-04 15:17:36 -0300

    add eager singleton

---
+++ src/designpatterns.gof/singleton/SingletonEager.java
@@ -0,0 +1,17 @@
+/**
+ * Singleton lazy "apressado"
+ * 
+ * @author azoozin
+ */
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstance() {
+        return instancia;
+    }
+}
