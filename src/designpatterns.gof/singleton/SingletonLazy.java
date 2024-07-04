7cf6079a5108913c02bd27cd874286c0567f105b
azoozin <mario.github@protonmail.com>
2024-07-04 15:17:27 -0300

    add lazy singleton

--- src/designpatterns.gof/singleton/SingletonLazy.java
+++ src/designpatterns.gof/singleton/SingletonLazy.java
@@ -5,4 +5,16 @@
  */
 public class SingletonLazy {
 
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstance() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
 }
