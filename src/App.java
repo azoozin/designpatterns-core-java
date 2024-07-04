7e59c390bc7fd5ec7d57d5591120c70bce535002
azoozin <mario.github@protonmail.com>
2024-07-04 15:28:52 -0300

    stuff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import designpatternsgof.singleton.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /**
+         * Test Singleton classes
+         */
+
+        SingletonLazy lazy = SingletonLazy.getInstance();
     }
 }
